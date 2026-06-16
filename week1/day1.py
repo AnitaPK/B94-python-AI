838d65468c51e3a14ae09eaaf8889f33fdd83dd0
AnitaPK <anita.khamitkar@gmail.com>
2026-06-16 19:55:56 +0530

    day1

--- week1/day1.py
+++ week1/day1.py
@@ -1,1 +1,93 @@
 print("*******Hello World*******")
+print("Your Name")
+print("Your favorite movie")
+print("Sports person")
+
+# single line comment 
+
+# variable = > used to store value 
+
+name = "Siddhant"
+marks = 98
+isStudent = True 
+
+# start with number
+# student-name 
+# class def for 
+
+# Data Types 
+
+# string = >str 
+# integer => int
+# Float =>float 
+# Boolean => bool 
+
+print(type(marks))
+print(type(isStudent))
+
+# Arithmetic operators
+x = 10 
+y = 20 
+
+print(x+y)  #30
+print(x-y) # -10
+print(x*y) # 200
+print(y/x) # 2
+print(x ** 3) #1000
+print(x % 2)  #=x/2. => 0.  
+print (y // x) #
+
+# 23/2 = 11.5 
+# 23//2 = 11
+
+# Comparison operators  
+
+# x == 5  
+# x != 5
+# x > 5
+# x >= 5 
+# x < 5
+# x <= 5
+# returns true or false 
+
+
+# logical operators 
+
+# and or not 
+
+# x == 5 and y == 5 
+
+#    1       1     = 1
+#    0       1     = 0
+#    1.      0     = 0
+#    0.      0     = 0
+
+# # if both are true => true 
+
+# x == 5 or y ==5 
+#    1       1    = 1 
+#    1       0    = 1
+#    0       1    = 1
+#    0       0    = 0 
+# # if one of then true =>true 
+
+# not isStudent =>false
+# not x==5   =>true 
+
+inputFromUser = input("Enter value")
+print(type(inputFromUser))
+
+num1 = int(input("Enter number"))
+print(type(num1))
+
+# simple interest 
+
+# si = (p * r * t) /100 
+
+# Mini Challenge 
+
+# Create a program that takes Name and Birth Year 
+# And Display 
+#     Welcome Siddhant
+#     Your Age is 21
+
